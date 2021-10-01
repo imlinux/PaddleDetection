d45df5fd743968b6e12e6fb49b7b2f2ff6ceb8fb
dsy <dongshuaiyang@calmore.com>
2021-10-01 19:35:03 +0800

    完善配置

--- dataset/no_voc/no_dataset.py
+++ dataset/no_voc/no_dataset.py
@@ -1,12 +1,20 @@
 import os
 import pathlib
 
-with open("train.txt", "w") as output:
-    for f in pathlib.Path("/home/dong/tmp/dataset/voc/no/no-PascalVOC-export/ImageSets/Main").glob("**/*train.txt"):
+root_dir = "/home/dong/tmp/dataset/voc/no"
 
-        with open(f) as input:
-            for line in input:
-                filename, _ = line.split(" ")
 
-                annotation_file = os.path.splitext(filename)[0] + ".xml"
-                print(f"/home/dong/tmp/dataset/voc/no/no-PascalVOC-export/JPEGImages/{filename} /home/dong/tmp/dataset/voc/no/no-PascalVOC-export/Annotations/{annotation_file}", file=output)
+def txt(mode="train"):
+    with open(f"{mode}.txt", "w") as output:
+        for f in pathlib.Path(f"{root_dir}/no-PascalVOC-export/ImageSets/Main").glob(f"**/*{mode}.txt"):
+
+            with open(f) as input:
+                for line in input:
+                    filename, _ = line.split(" ")
+
+                    annotation_file = os.path.splitext(filename)[0] + ".xml"
+                    print(f"{root_dir}/no-PascalVOC-export/JPEGImages/{filename} {root_dir}/no-PascalVOC-export/Annotations/{annotation_file}", file=output)
+
+
+txt(mode="train")
+txt(mode="val")
