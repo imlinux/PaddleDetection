0f26b54f5c847c6e1a4c1104518889fa359957ee
dsy <dongshuaiyang@calmore.com>
2021-10-05 23:38:06 +0800

    完善

--- dataset/no_voc/copy_file.py
+++ dataset/no_voc/copy_file.py
@@ -2,8 +2,10 @@ import shutil
 import pathlib
 import os
 
-for i in pathlib.Path('/home/dong/tmp/dataset/voc/zuowen').glob("**/*.xml"):
+for i in pathlib.Path('/home/dong/tmp/zuowen_resize').glob("**/*.xml"):
     *_, filename = str(i).split("/")
     file_path, ext = os.path.splitext(str(i))
-    file_path = file_path + ".jpg"
-    shutil.copyfile(file_path, "/home/dong/tmp/test/" + filename + ".jpg")
+    img_path = file_path + ".jpg"
+    ann_path = file_path + ".xml"
+    shutil.copyfile(img_path, "/home/dong/tmp/test/" + filename + ".jpg")
+    shutil.copyfile(ann_path, "/home/dong/tmp/test/" + filename + ".xml")
